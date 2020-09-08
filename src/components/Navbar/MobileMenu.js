962015b19c819720ca12c26aa43ece5add46e04b
Joanna Preston <joannappreston@gmail.com>
2020-09-08 16:11:22 -0700

    added navbar component

---
+++ src/components/Navbar/MobileMenu.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Link } from "react-router-dom";
+import Logout from "../Logout";
+import "./style.css";
+import LoginBtn from "../LoginBtn";
+
+function MobileMenu(props) {
+    return (
+        <div id="menu-canvas" uk-offcanvas="overlay: true">
+            <div className="uk-offcanvas-bar">
+                <button className="uk-offcanvas-close" type="button" uk-close="true"></button>
+                <ul className="uk-list">
+                    <li><Link to="/" className="nav-item mobile-nav-item" style={{ color: props.color }}>Home</Link></li>
+                    <li><a uk-toggle="target: #about-canvas" className="nav-item mobile-nav-item" style={{ color: props.color }}>About</a></li>
+                    <li><Link to="/when" className="nav-item mobile-nav-item" style={{ color: props.color }}>When</Link></li>
+                    <li><Link to="/where" className="nav-item mobile-nav-item" style={{ color: props.color }}>Where</Link></li>
+                    <li><Link to="/who" className="nav-item mobile-nav-item" style={{ color: props.color }}>Who</Link></li>
+                    <li className="uk-nav-header">My Account</li>
+                    <li><Link to="/dashboard" className="nav-item mobile-nav-item" style={{ color: props.color }}>Dashboard</Link></li>
+                    <li><LoginBtn classes="nav-item mobile-nav-item">Signup</LoginBtn></li>
+                    <li><LoginBtn classes="nav-item mobile-nav-item">Login</LoginBtn></li>
+                    <li><Logout classes="nav-item mobile-nav-item" /></li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+export default MobileMenu;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+const style = {
+    navbar: {
+        backgroundColor: "transparent"
+    },
+};
+
+function Navbar(props) {
+
+    return (
+        <div>
+            <nav className="uk-navbar-container uk-navbar navbar" style={style.navbar}>
+                <div className="uk-navbar-left">
+                    <ul className="uk-navbar-nav uk-visible@m">
+                        <li><Link to="/" className="nav-item" style={{ color: props.color }}>Home</Link></li>
+                        <li><a uk-toggle="target: #about-canvas" className="nav-item" style={{ color: props.color }}>About</a></li>
+                        <li><Link to="/about" className="nav-item" style={{ color: props.color }}>About</Link></li>
+                        <li><Link to="/services" className="nav-item" style={{ color: props.color }}>Services</Link></li>
+                        <li><Link to="/who" className="nav-item" style={{ color: props.color }}>Our Work</Link></li>
+                        <li><Link to="/who" className="nav-item" style={{ color: props.color }}>Careers</Link></li>
+                        <li><Link to="/FAQ" className="nav-item" style={{ color: props.color }}>FAQ</Link></li>
+                    </ul>
+                    <a className="uk-hidden@m" href="#menu-canvas" uk-toggle="target: #menu-canvas"><span className="menu-icon"
+                        uk-icon="icon: menu; ratio: 1.5"></span></a>
+                </div>
+            </nav>
+        </div>
+    )
+};
+
+export default Navbar;
