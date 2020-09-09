b836338ad0453cc43cc54b6ed94756187ebe364a
Joanna Preston <joannappreston@gmail.com>
2020-09-08 19:22:45 -0700

    initial banner/cover layout

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="uk-text-center uk-text-middle footer">
+            <p className="company-name">© Quality Creations</p>
+        </footer>
+    )
+};
+
+export default Footer;
--- client/src/pages/AboutPage/index.js
+++ client/src/pages/AboutPage/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./style.css";
 import Navbar from "../../components/Navbar";
+import image from "../../assets/images/about.jpg";
+import Footer from "../../components/Footer";
 
 function AboutPage() {
     return (
         <div>
-            <Navbar />
-            <h1>AboutPage</h1>
+            <div className="page-container">
+                <Navbar />
+                {/* <h1>AboutPage</h1> */}
+                <div className="pic-container">
+                    <div className="overlay"></div>
+                    <img src={image} alt="computer" />
+                    <div className="page-heading-container">
+                        <h1 className="page-heading">Who We Are</h1>
+                        <h5 className="page-subtitle">Some headline here, Lorem ipsum dolor sit ame lorem ”</h5>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 };
