8d6359c043c675fb4f740f7aaeea669573d7c30f
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-09-11 17:01:32 -0700

    home section technology card and model input

---
+++ client/src/components/HomeTechnologiesCard/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+function HomeTechnologiesCard(props) {
+  return (
+    <div class="background-image-technologies">
+      <div
+        class="uk-card uk-card-default uk-card-body background-image-technologies-card"
+        style={{
+          backgroundImage: `url(${props.src})`,
+        }}
+      >
+        <a href={`#${props.name.toLowerCase().split(" ").join("_")}`}>
+          <p>{props.name}</p>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default HomeTechnologiesCard;
--- client/src/components/HomeTechnologiesContainer/index.js
+++ client/src/components/HomeTechnologiesContainer/index.js
@@ -1,7 +1,50 @@
 import React from "react";
+import HomeTechnologiesCard from "../HomeTechnologiesCard";
+
+import web from "./images/webdevelopment.jpg";
+import software from "./images/software.jpg";
+import database from "./images/database.jpg";
+import maintence from "./images/maintence.jpg";
+import mobileapp from "./images/mobileapp.jpg";
+import apis from "./images/apis.jpg";
 
 function HomeTechnologiesContainer() {
-  return <div></div>;
+  let technologies = [
+    {
+      name: "Web Development",
+      src: web,
+    },
+    {
+      name: "Mobile Applications",
+      src: mobileapp,
+    },
+    {
+      name: "Software Development",
+      src: software,
+    },
+    {
+      name: "Databases",
+      src: database,
+    },
+    {
+      name: "APIs",
+      src: apis,
+    },
+    {
+      name: "Hosting and Maintence",
+      src: maintence,
+    },
+  ];
+  return (
+    <div
+      class="uk-grid-column-small uk-grid-row-large uk-child-width-1-3@s uk-text-center"
+      uk-grid="true"
+    >
+      {technologies.map((item) => {
+        return <HomeTechnologiesCard name={item.name} src={item.src} />;
+      })}
+    </div>
+  );
 }
 
 export default HomeTechnologiesContainer;
