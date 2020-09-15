73f2c1b29751552bc385d0f2ef800f733b5f6141
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-09-14 23:55:23 -0700

    careers page info add

--- client/src/pages/CareersPage/index.js
+++ client/src/pages/CareersPage/index.js
@@ -7,64 +7,116 @@ import Footer from "../../components/Footer";
 import ContentContainer from "../../components/ContentContainer";
 
 function CareersPage() {
-    return (
-        <div>
-            <Navbar />
-            <Cover
-                image={image}
-                heading="Work With Us"
-                subtitle="Some headline here, Lorem ipsum dolor sit ame lorem Lorem ipsum"
-                color="#065550d2"
-                right="5%"
-                justify="uk-text-right"
-            />
-            <ContentContainer>
-                <div class="uk-grid-match uk-child-width-expand@s uk-text-center" uk-grid="true">
-                    <div>
-                        <div class="uk-card uk-card-default uk-card-body uk-card-hover">
-                            <h3 className="card-header">Why Quality Creations</h3>
-                            <hr className="pink-hr" />
-                            <p className="card-p uk-text-justify">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel dui non elit vehicula tincidunt at ac quam. Donec hendrerit rhoncus dolor et luctus. Ut dignissim, enim congue tincidunt efficitur, lectus eros molestie enim, quis aliquam orci erat id magna. Integer nunc arcu, ultricies in mauris ac, fringilla varius ipsum. Etiam blandit nulla tellus, at euismod quam blandit et. Cras tristique dapibus vehicula. Morbi turpis purus, facilisis id pretium ac, accumsan hendrerit velit. Sed dui dolor, sodales eu orci fermentum, vulputate porttitor nisi. Ut at pharetra magna. Aenean mattis ut sem sed ultricies. Nam nibh ligula, maximus sed accumsan eu, elementum ac tellus.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel dui non elit vehicula tincidunt at ac quam. Donec hendrerit rhoncus dolor et luctus. Ut dignissim, enim congue tincidunt efficitur, lectus eros molestie enim, quis aliquam orci erat id magna. Integer nunc arcu, ultricies in mauris ac, fringilla varius ipsum. Etiam blandit nulla tellus, at euismod quam blandit et. Cras tristique dapibus vehicula. Morbi turpis purus, facilisis id pretium ac, accumsan hendrerit velit. Sed dui dolor, sodales eu orci fermentum, vulputate porttitor nisi. Ut at pharetra magna. Aenean mattis ut sem sed ultricies. Nam nibh ligula, maximus sed accumsan eu, elementum ac tellus.
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="uk-card uk-card-default uk-card-body uk-card-hover">
-                            <h3 className="card-header">Benefits</h3>
-                            <hr className="pink-hr" />
-                            <ul className="career-ul">
-                                <li className="career-li">Some benefit goes here lorem ipsum dolor sit amet</li>
-                                <li className="career-li">Some benefit goes here lorem ipsum dolor sit amet</li>
-                                <li className="career-li">Some benefit goes here lorem ipsum dolor sit amet</li>
-                            </ul>
-                        </div>
-                        <div class="uk-card uk-card-default uk-card-body uk-margin-top uk-card-hover">
-                            <h3 className="card-header">Open Positions</h3>
-                            <hr className="pink-hr" />
-                            <ul className="career-ul">
-                                <li className="career-li">Some job listing goes here lorem ipsum</li>
-                                <li className="career-li">Some job listing goes here lorem ipsum</li>
-                                <li className="career-li">Some job listing goes here lorem ipsum </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body uk-margin-top uk-text-center uk-card-hover">
-                        <h3 className="card-header uk-text-center">Interested in Working with us?</h3>
-                        <hr className="pink-hr" />
-                        <p className="card-p">
-                            Send your resume and cover letter to <a className="email-link" href="mailto:something@quality-creations.com">something@quality-creations.com.</a> Be sure to include WHY you want to work for Quality Creations!
-                        </p>
-                        <a className="uk-button uk-button-default apply-btn" href="mailto:something@quality-creations.com">Apply</a>
-                    </div>
-                </div>
-            </ContentContainer>
-            <Footer />
+  return (
+    <div>
+      <Navbar />
+      <Cover
+        image={image}
+        heading="Work With Us"
+        subtitle="Some headline here, Lorem ipsum dolor sit ame lorem Lorem ipsum"
+        color="#065550d2"
+        right="5%"
+        justify="uk-text-right"
+      />
+      <ContentContainer>
+        <div
+          class="uk-grid-match uk-child-width-expand@s uk-text-center"
+          uk-grid="true"
+        >
+          <div>
+            <div class="uk-card uk-card-default uk-card-body uk-card-hover">
+              <h3 className="card-header">Why Quality Creations</h3>
+              <hr className="pink-hr" />
+              <p className="card-p uk-text-justify">
+                Quality Creations was started for the sole purpose to
+                revolutionize the freelance and development industry by focusing
+                on the actual developers first through hands on training,
+                consulting, and team-focused approached to all projects. The
+                company utmost lives by the standard that a rising tide lifts
+                all boats. We strive to organically rise that tide daily through
+                direct trainings, hands-on practice and problem solving, which
+                creates a cumulative growth mindset throughout the group. By
+                being surrounded by a cast of motivated, skilled, and helping
+                individuals, it suddenly becomes fun, uplifting, and encouraging
+                to stay in the growth stage, where both the employee and company
+                see a direct return. To further reinforce the notion, the team
+                is constantly communicating through means of group-text,
+                conference calls, and Slack, where ideas, learning experiences,
+                and value information are constantly being transmitted between
+                all individuals. Just through the direct exposure to such an
+                uplifting environment automatically generates greater outcomes
+                for all parties involved. We solely believe that if we can
+                maintain such a status, then all finalized products will in turn
+                continue to be developed at greater rates and at greater
+                precision.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div class="uk-card uk-card-default uk-card-body uk-card-hover">
+              <h3 className="card-header">Benefits</h3>
+              <hr className="pink-hr" />
+              <ul className="career-ul">
+                <li className="career-li">
+                  Weekly trainings, seminars, and access to all notes, learning
+                  experiences, and motivational talks.
+                </li>
+                <li className="career-li">
+                  Access to Slack Channel, Custom Email, and Direct Network with
+                  other employees through Zoom, Group Chats, and frequent
+                  conference calls.
+                </li>
+                <li className="career-li">
+                  Hands-on learning with senior leaders improving skill,
+                  confidence, and production.
+                </li>
+              </ul>
+            </div>
+            <div class="uk-card uk-card-default uk-card-body uk-margin-top uk-card-hover">
+              <h3 className="card-header">Open Positions</h3>
+              <hr className="pink-hr" />
+              <ul className="career-ul">
+                <li className="career-li">
+                  Some job listing goes here lorem ipsum
+                </li>
+                <li className="career-li">
+                  Some job listing goes here lorem ipsum
+                </li>
+                <li className="career-li">
+                  Some job listing goes here lorem ipsum{" "}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    )
-};
+        <div>
+          <div class="uk-card uk-card-default uk-card-body uk-margin-top uk-text-center uk-card-hover">
+            <h3 className="card-header uk-text-center">
+              Interested in Working with us?
+            </h3>
+            <hr className="pink-hr" />
+            <p className="card-p">
+              Send your resume and cover letter to{" "}
+              <a
+                className="email-link"
+                href="mailto:something@quality-creations.com"
+              >
+                something@quality-creations.com.
+              </a>{" "}
+              Be sure to include WHY you want to work for Quality Creations!
+            </p>
+            <a
+              className="uk-button uk-button-default apply-btn"
+              href="mailto:something@quality-creations.com"
+            >
+              Apply
+            </a>
+          </div>
+        </div>
+      </ContentContainer>
+      <Footer />
+    </div>
+  );
+}
 
 export default CareersPage;
